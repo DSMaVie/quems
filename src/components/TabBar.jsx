ed91869ff5f30b811c115d1996b78707709af7da
Max Vieweg <maximilian.manfred.vieweg@rwth-aachen.de>
2021-02-16 19:04:21 +0100

    experimented with global settings e.g. locale

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { AppBar, Tabs, Tab } from '@material-ui/core';
-
+import { AppBar, Tabs, Tab, ButtonGroup, IconButton, Toolbar } from '@material-ui/core';
+import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
+import { IconFlagDE } from 'material-ui-flags';
 function TabBar(props) {
   const [currentTab, setTab] = React.useState(props.listOfTabs[0]);
+  const globalSettings = React.useContext(GlobalSettings);
 
   const handleTabChange = (event, newTab) => {
     setTab(newTab);
@@ -12,15 +14,25 @@ function TabBar(props) {
   return (
     <div>
       <AppBar position="static">
-        <Tabs
-          value={currentTab}
-          onChange={handleTabChange}
-          centered //small displays should use variant="fullWidth" instead
-        >
-          {props.listOfTabs.map((tab) => (
-            <Tab key={tab} label={tab} />
-          ))}
-        </Tabs>
+        <Toolbar>
+          <Tabs
+            value={currentTab}
+            onChange={handleTabChange}
+            centered //small displays should use variant="fullWidth" instead
+          >
+            {props.listOfTabs.map((tab) => (
+              <Tab key={tab} label={tab} />
+            ))}
+            <ButtonGroup variant="text" color="secondary">
+              <IconButton>
+                <WbIncandescentIcon />
+              </IconButton>
+              <IconButton>
+                <IconFlagDE />
+              </IconButton>
+            </ButtonGroup>
+          </Tabs>
+        </Toolbar>
       </AppBar>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { deDE, enUS } from '@material-ui/core/locale';
+
+const GlobalSettings = React.createContext({
+  darkMode: false,
+  locale: 'enUS',
+});
+
+const locales = {
+  enUs: enUS,
+  deDE: deDE,
+};
+
+const theme = createMuiTheme(
+  {
+    palette: {
+      type: GlobalSettings.darkMode,
+    },
+  },
+  locales[GlobalSettings.locale],
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
